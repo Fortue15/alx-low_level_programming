f8bba2cb18175f9b139f762ad4b1dc9286847be1
Fortue15 <Fortuechigogo3@gmail.com>
2022-09-08 18:18:18 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-printing
+ * Return:always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+}
