5ee9340b559597b91a335d8000ef88773573e2af
Fortue15 <Fortuechigogo3@gmail.com>
2022-09-11 09:46:27 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,11 @@
 *Return: Always 0.
 */
 int main(void)
-
 {
-       char letter;
+      char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
-	putchar('\n');
-return (0);
+      for (letter = 'a'; letter <= 'z'; letter++)
+      putchar(letter);
+      putchar('\n');
+      return (0);
 }
