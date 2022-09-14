5f8687909dede9b7a8ee61b531b8ceab8b026627
Fortue15 <Fortuechigogo3@gmail.com>
2022-09-14 16:01:21 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,17 +6,13 @@
  */
 int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-		n = n;
+		return (n);
 	}
-	else if (n > 0)
+	else
 	{
 		n = -n;
+		return (n);
 	}
-	else if (n == 0)
-	{
-		n = 0;
-	}
-	return (n);
-}
+}	
