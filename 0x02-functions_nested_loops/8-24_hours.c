39c0899a3366afe85d579e12b8939bc5642a91ec
Fortue15 <Fortuechigogo3@gmail.com>
2022-09-14 18:29:50 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* jack_bauer - > prints every minute of the day
+* jack_bauer - > prints every minute of the day.
 */
 void jack_bauer(void)
 {
 	int min, hr;
 
-	for (hr = 0; hr <=23; ++hr)
+	for (hr = 0; hr <= 23; ++hr)
 	{
-		for (main = 0; min <= 59; ++min)
+		for (min = 0; min <= 59; ++min)
 		{
 			_putchar ((hr / 10) + 48);
 			_putchar ((hr % 10) + 48);
@@ -18,4 +18,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-}	
+}
