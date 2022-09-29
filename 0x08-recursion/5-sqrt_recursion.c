c443dd57263aa0693ac037dfdedbe52bdb58ebec
Fortue15 <Fortuechigogo3@gmail.com>
2022-09-29 15:16:59 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,14 +29,13 @@ return (-1);
  */
 int _sqrt_recursion(int n)
 {
-	int = 0;
+	int i = 0;
 
-	if (i < 0) /*if n is negative*/
+	if (n < 0) /*if n is negative*/
 		return (-1);
 
 	else
 	{
 		return (_evaluate(i, n)); /*Recursive call*/
 	}
-
 }
